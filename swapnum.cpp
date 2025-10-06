6d644dee4a5d617e6ad16383bcc9b3d2ed800122
kaviyashree <kaviyakaruppanasamy@gmail.com>
2025-10-06 10:25:02 +0530

    Create swapnum.cpp

---
+++ swapnum.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+int a=10,b=20,temp;
+cout<<"Before swapping: a="<<a<<",b="<<b<<endl;
+temp=a;
+a=b;
+b=temp;
+cout<<"After swapping: a="<<a<<",b="<<b<<endl;
+return 0;
+}
